a5a43599889f1e7f35f0433f198acfe7bcc7cd55
Samuelisusername <115652284+Samuelisusername@users.noreply.github.com>
2024-03-06 17:16:41 +0100

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -28,6 +28,24 @@ column2 = 'bob' WHERE id =1;
 
 DELETE FROM myTable WHERE id = 1; 
 
+// NOW THE SELECTS
+
+SELECT column1, AVG() FROM myTable JOIN otherTable ON company_id = id WHERE column2 LIKE '%Hell0_' GROUP BY company_id...
+
+
+
+
+  
+  SELECT DISTINCT column, AGG_FUNC(column_or_expression), …
+FROM mytable
+    JOIN another_table
+      ON mytable.column = another_table.column
+    WHERE constraint_expression
+    GROUP BY column
+    HAVING constraint_expression
+    ORDER BY column ASC/DESC
+    LIMIT count OFFSET COUNT;
+
 
 
 
